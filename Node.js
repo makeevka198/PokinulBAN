9cd3521797bcfde655cc9ea50253ff339eda2253
makeevka198 <32690319+makeevka198@users.noreply.github.com>
2025-11-28 22:14:57 +0300

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const app = express();
+import os
+from flask import Flask
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
